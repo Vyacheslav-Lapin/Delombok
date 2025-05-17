4ce7b3c369db623fcaa4d2f680c64a0b7301374d
GitHub Action <action@github.com>
2025-05-17 13:21:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/vyacheslavlapin/delombok/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/vyacheslavlapin/delombok/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.vyacheslavlapin.delombok.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vyacheslavlapin.delombok.MyBundle
+import com.github.vyacheslavlapin.delombok.services.MyProjectService
 import javax.swing.JButton
 
 
